22f2dd35e2fdca1bbb9b052a3c5612df36ece944
Frey-r <eduardo.bachmann.f@gmail.com>
2024-02-04 19:04:59 -0300

    Create:APP ADD: nav

---
+++ src/components/NavComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <div :id="iconLogo">
+      <button :type="button" @click="openNav">
+        <img :src="require('@/assets/inguz.svg')" alt="Icon Logo" />
+      </button>
+    </div>
+  </nav>
+</template>
+<style>
+:root {
+  --inguz_green: #99ca3b;
+  --inguz_pink: #b93ec6;
+}
+button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: white;
+  border-radius: 100%;
+  border: solid black 6px;
+  height: 80px;
+  width: 80px;
+  img {
+    width: 50px;
+    height: auto;
+    background: transparent;
+  }
+}
+button:hover {
+  background: var(--inguz_pink);
+}
+</style>
+<script setup lang="ts"></script>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,44 @@
+<template>
+  <NavComponent></NavComponent>
+  <div class="test-msg">
+    <p>
+      {{ msg }}
+    </p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import NavComponent from "@/components/NavComponent.vue";
+export default {
+  name: "IndexPage",
+  components: { NavComponent },
+  data() {
+    return { msg: "pass" };
+  },
+  methods: {
+    getResponse() {
+      const path = "http://localhost:5000/index";
+      axios
+        .get(path)
+        .then((res) => {
+          console.log(res.data);
+          this.msg = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+  created() {
+    this.getResponse();
+  },
+};
+</script>
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #3a3a3a;
+}
+</style>

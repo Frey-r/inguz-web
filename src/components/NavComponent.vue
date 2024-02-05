157f20bbe57b709abb0c328e36245679c2402ecb
Frey-r <eduardo.bachmann.f@gmail.com>
2024-02-04 22:14:42 -0300

    ADD: nav base styles

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,9 +1,17 @@
 <template>
   <nav>
-    <div :id="iconLogo">
+    <div :id="iconLogo"> /* hay que colocarlo z-index:2; */
       <button :type="button" @click="openNav">
         <img :src="require('@/assets/inguz.svg')" alt="Icon Logo" />
       </button>
+    </div>
+    <div class="nav-bar">
+      <ul>
+        <li>
+          <a :href="indexView">INICIO</a>
+        </li>
+        <li></li>
+      </ul>
     </div>
   </nav>
 </template>
@@ -11,6 +19,21 @@
 :root {
   --inguz_green: #99ca3b;
   --inguz_pink: #b93ec6;
+}
+nav {
+  display: flex;
+  align-items: center;
+  height: 80px;
+  font-size: 32px;
+  font-weight: bolder;
+  &:before {
+    content: "";
+    display: block;
+    width: 2px;
+    height: 60%;
+    background-color: black;
+    margin-right: 20px;
+  }
 }
 button {
   display: flex;
@@ -30,5 +53,25 @@ button {
 button:hover {
   background: var(--inguz_pink);
 }
+.nav-bar {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  width: 80%;
+  background-color: black;
+  li {
+    list-style-type: none;
+  }
+  ul {
+    display: flex;
+    align-items: center;
+  }
+  a {
+    color: white;
+    text-decoration: none;
+  }
+}
 </style>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import indexView from "@/views/IndexView.vue";
+</script>

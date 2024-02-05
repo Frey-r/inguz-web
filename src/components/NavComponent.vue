b54dc96a797dd74329ceb321c88b664d4d5daa27
Frey-r <eduardo.bachmann.f@gmail.com>
2024-02-05 19:07:24 -0300

    ADD: nav styles {forma, medida,hover-color,funcionalidad del boton}

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <div :id="iconLogo"> /* hay que colocarlo z-index:2; */
+    <div class="icon_logo">
       <button :type="button" @click="openNav">
         <img :src="require('@/assets/inguz.svg')" alt="Icon Logo" />
       </button>
@@ -8,9 +8,16 @@
     <div class="nav-bar">
       <ul>
         <li>
-          <a :href="indexView">INICIO</a>
+          <a @mouseenter="checkWidth" @mouseleave="resetWidth">INICIO</a>
         </li>
-        <li></li>
+        <li>
+          <a @mouseenter="checkWidth" @mouseleave="resetWidth">CONTACTANOS</a>
+        </li>
+        <li>
+          <a @mouseenter="checkWidth" @mouseleave="resetWidth"
+            >SOBRE NOSOTROS</a
+          >
+        </li>
       </ul>
     </div>
   </nav>
@@ -21,19 +28,15 @@
   --inguz_pink: #b93ec6;
 }
 nav {
+  margin: 70px;
   display: flex;
   align-items: center;
   height: 80px;
   font-size: 32px;
   font-weight: bolder;
-  &:before {
-    content: "";
-    display: block;
-    width: 2px;
-    height: 60%;
-    background-color: black;
-    margin-right: 20px;
-  }
+}
+.icon_logo {
+  z-index: 2;
 }
 button {
   display: flex;
@@ -44,34 +47,93 @@ button {
   border: solid black 6px;
   height: 80px;
   width: 80px;
+  &:hover {
+    img {
+      transform: scale(1.2);
+    }
+  }
   img {
     width: 50px;
     height: auto;
     background: transparent;
   }
 }
-button:hover {
-  background: var(--inguz_pink);
-}
 .nav-bar {
-  display: flex;
+  display: none;
   align-items: center;
   height: 100%;
-  width: 80%;
   background-color: black;
   li {
     list-style-type: none;
   }
   ul {
     display: flex;
-    align-items: center;
+    justify-content: space-evenly;
+    width: 100%;
   }
   a {
     color: white;
     text-decoration: none;
+    position: relative;
+    display: inline-block;
+    z-index: 2;
+
+    &:after {
+      content: "";
+      display: block;
+      position: fixed;
+      height: 20px;
+      z-index: -1;
+      transform: translate(7px, -21px);
+      transition: width 0.3s ease;
+    }
+    &.highlighted::after {
+      width: var(--text-width, 0);
+      background-color: var(--inguz_pink);
+    }
+  }
+}
+.open {
+  display: flex;
+  width: 80%;
+  &:before {
+    content: "";
+    display: block;
+    width: 40px;
+    height: 80px;
+    background-color: black;
+    margin-right: 20px;
+    transform: translateX(-38px);
+  }
+  &:after {
+    content: "";
+    display: block;
+    width: 0;
+    height: 0;
+    border-right: 80px solid transparent;
+    border-top: 80px solid black;
+    position: fixed;
+    transform: translateX(1903px);
   }
 }
 </style>
 <script setup lang="ts">
-import indexView from "@/views/IndexView.vue";
+function openNav(event: MouseEvent): void {
+  let nav_bar = document.getElementsByClassName("nav-bar")[0];
+  if (nav_bar) {
+    nav_bar.classList.toggle("open");
+  }
+}
+function checkWidth(event: MouseEvent) {
+  const target = event.currentTarget as HTMLElement;
+  const textWidth = target.offsetWidth;
+
+  // Aplicar clase para resaltar y establecer la variable CSS
+  target.classList.add("highlighted");
+  target.style.setProperty("--text-width", `${textWidth}px`);
+}
+function resetWidth(event: MouseEvent) {
+  const target = event.currentTarget as HTMLElement;
+  target.classList.remove("highlighted");
+}
 </script>
